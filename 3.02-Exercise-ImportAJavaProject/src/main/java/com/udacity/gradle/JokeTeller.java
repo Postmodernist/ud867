3f9153e349d266d06b36fc9cdf7eb5a87aa0c5a0
Alex Baryzhikov <aleksiarts@gmail.com>
2018-08-05 21:03:57 +0300

    Exercises 3.01, 3.02, 3.05

--- 3.02-Exercise-ImportAJavaProject/src/main/java/com/udacity/gradle/JokeTeller.java
+++ 3.02-Exercise-ImportAJavaProject/src/main/java/com/udacity/gradle/JokeTeller.java
@@ -3,7 +3,7 @@ package com.udacity.gradle;
 
 public class JokeTeller {
 
-    public static void main(String[] args){
-        System.out.println("Java. Write once, debug everywhere.");
-    }
+  public static void main(String[] args) {
+    System.out.println("Java. Write once, debug everywhere.");
+  }
 }
